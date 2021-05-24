1354bbdb3698924f8a96301770cb0b24acae40bb
Dimitar Andreev <d.andreev90@gmail.com>
2021-05-24 22:00:52 +0300

    Add BM25

--- src/node_rank_fns.cc
+++ src/node_rank_fns.cc
@@ -111,11 +111,38 @@ std::vector<double> NodeRankFns::GetTfIdf(std::vector<std::string> &documents, s
     return scores;
 }
 
+std::vector<double> NodeRankFns::GetBM25(std::vector<std::string> &documents, std::vector<std::string> &terms) {
+    double df = this->GetDocumentFrequency(documents, terms);
+    std::vector<double> scores;
+    int documentLengthSum = 0;
+    int n = documents.size();
+    for (auto & document : documents) {
+        documentLengthSum += document.size();
+    }
+    double averageDocumentLength = (double)documentLengthSum / n;
+    double k = 1;
+    double b = 0.5;
+    for (const auto& doc : documents) {
+        double documentLength = doc.size();
+        auto words = this->SplitString(doc);
+        double tf = this->GetTermFrequency(words, terms);
+        // Fancy tf accounts for term saturation and document length.
+        double fancyTf = tf / (tf + k * (1 - b + b * documentLength / averageDocumentLength));
+        // Fancy (not so fancy) idf considers less a term if it's in most of the documents.
+        double fancyIdf = log10(1 + (n - df + .5) / (df + .5));
+        scores.push_back(abs(fancyTf * fancyIdf));
+    }
+
+    return scores;
+}
+
 /**
  * Get TF-IDF scores
  * @param info [documents file path, terms file path]
  * @returns tf-idf score for each of the documents
  * @example nodeRankFns.tfIdf('./documents/file/path.txt', './terms/file/path.txt')
+ * O(N * M) ST (where N is the number of documents and M is the number of terms)
+ * O(N) T (where N is the number of documents
  */
 Napi::Value NodeRankFns::TfIdf(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
@@ -135,10 +162,45 @@ Napi::Value NodeRankFns::TfIdf(const Napi::CallbackInfo& info) {
         return env.Null();
     }
 
-    std::vector<std::string> documents = this->ParseInputFile(info[0].As<Napi::String>().Utf8Value());
-    std::vector<std::string> terms = this->ParseInputFile(info[1].As<Napi::String>().Utf8Value());
+    auto documents = this->ParseInputFile(info[0].As<Napi::String>().Utf8Value());
+    auto terms = this->ParseInputFile(info[1].As<Napi::String>().Utf8Value());
 
     auto scores = this->GetTfIdf(documents, terms);
+    auto result = this->VectorToNapiArray(env, scores);
+
+    return reinterpret_cast<Napi::Value &&>(result);
+}
+
+/**
+ * Get BM25 scores
+ * @param info [documents file path, terms file path]
+ * @returns tf-idf score for each of the documents
+ * @example nodeRankFns.bm25('./documents/file/path.txt', './terms/file/path.txt')
+ * O(N * M) ST (where N is the number of documents and M is the number of terms)
+ * O(N) T (where N is the number of documents
+ */
+Napi::Value NodeRankFns::BM25(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    if (info.Length() < 2) {
+        Napi::TypeError::New(env, "Wrong number of arguments")
+                .ThrowAsJavaScriptException();
+        return env.Null();
+    }
+    if (!info[0].IsString()) {
+        Napi::TypeError::New(env, "Documents file path is not valid")
+                .ThrowAsJavaScriptException();
+        return env.Null();
+    }
+    if (!info[1].IsString()) {
+        Napi::TypeError::New(env, "Terms file path is not valid")
+                .ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    auto documents = this->ParseInputFile(info[0].As<Napi::String>().Utf8Value());
+    auto terms = this->ParseInputFile(info[1].As<Napi::String>().Utf8Value());
+
+    auto scores = this->GetBM25(documents, terms);
     auto result = this->VectorToNapiArray(env, scores);
 
     return reinterpret_cast<Napi::Value &&>(result);
@@ -147,6 +209,7 @@ Napi::Value NodeRankFns::TfIdf(const Napi::CallbackInfo& info) {
 Napi::Function NodeRankFns::GetClass(Napi::Env env) {
     return DefineClass(env, "NodeRankFns", {
         NodeRankFns::InstanceMethod("tfIdf", &NodeRankFns::TfIdf),
+        NodeRankFns::InstanceMethod("bm25", &NodeRankFns::BM25),
     });
 }
 
--- test/index.js
+++ test/index.js
@@ -3,14 +3,22 @@ const NodeRankFns = require('../dist/binding.js');
 
 const documentsFilePath = './test/documents.txt';
 const termsFilePath = './test/terms.txt';
+const instance = new NodeRankFns();
 
 function testTfIdf() {
-    const instance = new NodeRankFns();
     const res = instance.tfIdf(documentsFilePath, termsFilePath);
-    console.log('Res: ', res);
+    console.log('TF-IDF: ', res);
 
     assert.deepEqual(res, [0, 0.12901285528456335]);
 }
-
 assert.doesNotThrow(testTfIdf, undefined, 'testBasic threw an exception');
+
+function testBM25() {
+    const res = instance.bm25(documentsFilePath, termsFilePath);
+    console.log('BM25: ', res);
+
+    assert.deepEqual(res, [0, 0.07811157572119272]);
+}
+assert.doesNotThrow(testBM25, undefined, 'testBM25 threw an exception');
+
 console.log('Tests passed - everything looks OK!');
